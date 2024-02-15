07e7145da2b3564ea780089511c9e8b7e12624bb
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-02-16 01:52:05 +0800

    Create 2942.cpp

---
+++ 3000/2942.cpp
@@ -0,0 +1,20 @@
+// 2942. Find Words Containing Character
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> result;
+
+        for (int i=0; i<words.size(); i++)
+        {
+            for (int j=0; j<words[i].length(); j++)
+            {
+                if (words[i][j] == x) 
+                {
+                    result.push_back(i);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
