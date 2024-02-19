8392835ce8abb1b1d825a8e7c34d617ac8400299
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-02-19 13:52:22 +0800

    Update 0231.cpp

--- 1000/0231.cpp
+++ 1000/0231.cpp
@@ -4,13 +4,12 @@ public:
     bool isPowerOfTwo(int n) {
         // count number of 1 in binary format
         if (n<1) return false;
-        int cnt = 0;
-        while (n)
+        int cnt = n&1;
+        while (n>>=1)
         {
             cnt += (n&1);
             if (cnt>1) return false;
-            n/=2;
         }
-        return (cnt==1);
+        return true;
     }
 };
