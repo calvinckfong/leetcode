28c9cc0e207c41d4fdca9b4ab1c2c0158bfac9a0
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-03-03 19:43:18 +0800

    Update 1963.cpp

--- 2000/1963.cpp
+++ 2000/1963.cpp
@@ -5,9 +5,11 @@ public:
         int closeCnt=0, maxCloseCnt=0;
         for (int i=0; i<s.size(); i++)
         {
-            if (s[i]==']') closeCnt++;
+            if (s[i]==']') {
+                closeCnt++;
+                if (closeCnt>maxCloseCnt) maxCloseCnt = closeCnt;
+            }
             else closeCnt--;
-            if (closeCnt>maxCloseCnt) maxCloseCnt = closeCnt;
         }
         return (maxCloseCnt+closeCnt+1)/2;
     }
