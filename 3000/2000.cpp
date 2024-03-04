a1d7cd5e47bb2cd4ff7a6d45134718676eaf21c4
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-03-04 16:48:09 +0800

    Create 2000.cpp

---
+++ 3000/2000.cpp
@@ -0,0 +1,11 @@
+// 2000. Reverse Prefix of Word
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        auto p = word.find(ch);
+        if (p != string::npos)
+            reverse(word.begin(), word.begin()+p+1);
+
+        return word;
+    }
+};
