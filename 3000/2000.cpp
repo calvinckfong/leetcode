145d447e5b05d4127fc9aa5a284a2823609b3e5d
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-05-01 10:42:51 +0800

    Update 2000.cpp
    Provide another solution

--- 3000/2000.cpp
+++ 3000/2000.cpp
@@ -2,10 +2,25 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
+        // Solution 1
         auto p = word.find(ch);
         if (p != string::npos)
             reverse(word.begin(), word.begin()+p+1);
 
         return word;
+        
+        /* Solution 2
+        for (int i=0; i<word.size(); i++) {
+            if (ch == word[i]) {
+                int j=0;
+                while (j<i) {
+                    swap(word[j++], word[i--]);
+                }
+                break;
+            }
+        }
+
+        return word;
+        */
     }
 };
