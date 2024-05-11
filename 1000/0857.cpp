6001b754b5ce941f3b6dc4b11ee7cbf45a4a28cf
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-05-11 15:17:14 +0800

    Update 0857.cpp

--- 1000/0857.cpp
+++ 1000/0857.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
         int n=quality.size();
-        // compute and sort the work-quality ratio
+        // compute and sort the wage-quality ratio
         vector<pair<double, int>> wq_ratio(n);
         for (int i=0; i<n; i++)
             wq_ratio[i] = {1.0*wage[i]/quality[i], i};
@@ -12,26 +12,28 @@ public:
         priority_queue<int> pq;
         int qSum = 0;
         double maxRatio = 0.0, result;
-        for (int i=0; i<n; i++) {
-            double ratio = wq_ratio[i].first;
-            int index = wq_ratio[i].second;
-            if (pq.size()<k) {
-                // before k workers are chosen, 
-                // keep adding to queue and updating the pay amount
-                qSum += quality[index];
-                pq.push(quality[index]);
-                maxRatio = max(maxRatio, ratio);
-                result = maxRatio * qSum;
-            } else {
-                // add new worker to queue, and
-                // pop the one with less quality
-                maxRatio = max(maxRatio, ratio);
-                qSum = qSum - pq.top() + quality[index];
-            
-                pq.pop();
-                pq.push(quality[index]);
-                result = min(result, maxRatio*qSum);
-            }
+        // add workers according to the wage-quality ratio
+        // i.e. choose cheaper worker first
+        
+        // before k workers are chosen, 
+        // keep adding to queue and updating the pay amount
+        for (int i=0; i<k; i++) {
+            auto[ratio, index] = wq_ratio[i];
+            qSum += quality[index];
+            pq.push(quality[index]);
+            maxRatio = max(maxRatio, ratio);
+            result = maxRatio * qSum;
+        }
+
+        // add new worker to queue, and
+        // pop the one with less quality
+        for (int i=k; i<n; i++) {
+            auto[ratio, index] = wq_ratio[i];
+            maxRatio = max(maxRatio, ratio);
+            qSum = qSum - pq.top() + quality[index];
+            pq.pop();
+            pq.push(quality[index]);
+            result = min(result, maxRatio*qSum);
         }
 
         return result;
