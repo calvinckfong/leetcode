20b37584eaac9b02a0fabb020d016d3d69ee4401
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-05-31 10:48:43 +0800

    Create 3131.cpp

---
+++ 4000/3131.cpp
@@ -0,0 +1,8 @@
+// 3131. Find the Integer Added to Array I
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        return  *min_element(nums2.begin(), nums2.end()) - 
+            *min_element(nums1.begin(), nums1.end());
+    }
+};
