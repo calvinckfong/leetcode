666951ebbf7e0ad582dd26fee9465f3cade5b633
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-06-02 12:42:21 +0800

    Update 0344.cpp

--- 1000/0344.cpp
+++ 1000/0344.cpp
@@ -2,8 +2,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        for (int i=0; i<(n+1)/2; i++)
-            swap(s[i], s[n-i-1]);
+        int l=0, r = s.size()-1;
+        while (l<r) {
+            swap(s[l++], s[r--]);
+        }
     }
 };
