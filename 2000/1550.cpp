14bc42051ed82d51acc574bf42d5a2de3fc6f244
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-07-02 00:45:06 +0800

    Create 1550.cpp

---
+++ 2000/1550.cpp
@@ -0,0 +1,13 @@
+// 1550. Three Consecutive Odds
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if (arr.size()<3) return false;
+        for (int i=0; i<arr.size()-2; i++) {
+            if (arr[i]%2) {
+                if (arr[i+1]%2 && arr[i+2]%2) return true;
+            } 
+        }
+        return false;
+    }
+};
