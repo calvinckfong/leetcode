6a8cbf1ffc3fcaf10e838ec8287a293aba8533df
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-12-19 21:38:14 +0800

    Create 0769.cpp

---
+++ 1000/0769.cpp
@@ -0,0 +1,21 @@
+// 769. Max Chunks To Make Sorted
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        stack<int> st;
+
+        for (int i=0; i<n; i++) {
+            if (st.empty() || arr[i]>st.top()) {
+                st.push(arr[i]);
+            } else {
+                int top = st.top();
+                while (!st.empty() && arr[i]<st.top()) {
+                    st.pop();
+                }
+                st.push(top);
+            }
+        }
+        return st.size();
+    }
+};
