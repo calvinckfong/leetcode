3cc2cd4ecceb3f89f54ac2bd43423a946d278b04
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-03-05 23:08:11 +0800

    Update 2579.cpp

--- 3000/2579.cpp
+++ 3000/2579.cpp
@@ -2,10 +2,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long result = 0;
-        while (--n>0) {
-            result += n;
-        }
+        long long result = (long long)(n-1)*(long long)(n)/2;
         return result * 4 + 1;
     }
 };
