e0426cd0774ee988d9f10472038ebb311fca0b9b
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-05-15 19:38:21 +0800

    Create 2900.cpp

---
+++ 3000/2900.cpp
@@ -0,0 +1,15 @@
+// 2900. Longest Unequal Adjacent Groups Subsequence I
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+        vector<string> result;
+
+        for (int i=0; i<n; i++) {
+            if (i==0 || groups[i]!=groups[i-1]) {
+                result.emplace_back(words[i]);
+            }
+        }
+        return result;
+    }
+};
