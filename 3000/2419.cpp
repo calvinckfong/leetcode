210ecfe805821a0c1af15dd359c1c71cef3e25dd
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-07-30 20:33:31 +0800

    Update 2419.cpp

--- 3000/2419.cpp
+++ 3000/2419.cpp
@@ -2,17 +2,17 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int longestSubarrayLen=0, maxVal=0, len=0;
+        int res=0, maxVal=0, len=0;
 
-        for (int val: nums) {
+        for (int& val: nums) {
             if (val > maxVal) {
                 maxVal = val;
-                longestSubarrayLen = len = 0;
+                res = len = 0;
             }
             len = (maxVal == val) ? (len+1) : 0;
-            longestSubarrayLen = max(longestSubarrayLen, len);
+            res = max(res, len);
         }
 
-        return longestSubarrayLen;
+        return res;
     }
 };
