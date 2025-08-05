c18fda303953fdc5759617d6b911a735e6d88dd5
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-08-05 19:46:54 +0800

    Create 3477.cpp

---
+++ 4000/3477.cpp
@@ -0,0 +1,19 @@
+// 3477. Fruits Into Baskets II
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int res=0, n = baskets.size();
+        for (int& f: fruits) {
+            int unplaced = 1;
+            for (int i=0; i<n; i++) {
+                if (f <= baskets[i]) {
+                    baskets[i] = 0;
+                    unplaced = 0;
+                    break;
+                }
+            }
+            res += unplaced;
+        }
+        return res;
+    }
+};
