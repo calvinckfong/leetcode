80b7534beaaa851a3232df9e5167915ce9c6e0df
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-08-06 23:17:11 +0800

    Create 3479.cpp

---
+++ 4000/3479.cpp
@@ -0,0 +1,75 @@
+// 3479. Fruits Into Baskets III
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        m_baskets = baskets;
+        int m = baskets.size();
+        int count = 0;
+        if (m == 0) {
+            return fruits.size();
+        }
+        build(1, 0, m - 1);
+        for (int i = 0; i < m; i++) {
+            int l = 0, r = m - 1, res = -1;
+            while (l <= r) {
+                int mid = (l + r) >> 1;
+                if (query(1, 0, m - 1, 0, mid) >= fruits[i]) {
+                    res = mid;
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (res != -1 && baskets[res] >= fruits[i]) {
+                update(1, 0, m - 1, res, INT_MIN);
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+private:
+    int segTree[400007];
+    vector<int> m_baskets;
+
+    void build(int p, int l, int r) {
+        if (l == r) {
+            segTree[p] = m_baskets[l];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        int p2 = p<<1;
+        build(p2, l, mid);
+        build(p2+1, mid+1, r);
+        segTree[p] = max(segTree[p2], segTree[p2+1]);
+    }
+
+    int query(int p, int l, int r, int ql, int qr) {
+        if (ql > r || qr < l) {
+            return INT_MIN;
+        }
+        if (ql <= l && r <= qr) {
+            return segTree[p];
+        }
+        int mid = (l + r) >> 1;
+        int p2 = p<<1;
+        return max(query(p2, l, mid, ql, qr),
+                   query(p2+1, mid+1, r, ql, qr));
+    }
+
+    void update(int p, int l, int r, int pos, int val) {
+        if (l == r) {
+            segTree[p] = val;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        int p2 = p<<1;
+        if (pos <= mid) {
+            update(p2, l, mid, pos, val);
+        } else {
+            update(p2+1, mid + 1, r, pos, val);
+        }
+        segTree[p] = max(segTree[p2], segTree[p2+1]);
+    }
+};
