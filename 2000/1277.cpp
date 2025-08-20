80e469539d7a2f189065965454ee45b16cbb813f
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-08-20 23:05:35 +0800

    Update 1277.cpp

--- 2000/1277.cpp
+++ 2000/1277.cpp
@@ -3,17 +3,21 @@ class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
         int result = 0;
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
+        for(int i=0;i<m;i++)    dp[i][0]=matrix[i][0];
+        for(int j=0;j<n;j++)    dp[0][j]=matrix[0][j];
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
                 if (matrix[i][j]) {
-                    dp[i+1][j+1] = 1 + min({dp[i][j], dp[i+1][j], dp[i][j+1]});
-                    result += dp[i+1][j+1];
+                    dp[i][j] = 1 + min({dp[i-1][j-1], dp[i][j-1], dp[i-1][j]});
                 }
             }
         }
+
+        for (int i=0; i<m; i++)
+            result += accumulate(dp[i].begin(), dp[i].end(), 0);
         return result;
     }
 };
