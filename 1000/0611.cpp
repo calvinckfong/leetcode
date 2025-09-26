e02003342e283c75db50870f13d51717ae9c9867
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-09-26 22:37:36 +0800

    Create 0611.cpp

---
+++ 1000/0611.cpp
@@ -0,0 +1,19 @@
+// 611. Valid Triangle Number
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0, n = nums.size();
+        for (int i=0; i<n-2; i++)
+        {
+            int k = i+2;
+            for (int j=i+1; j<n-1 && nums[i]!=0; j++) 
+            {
+                int sum = nums[i] + nums[j];
+                while (k<n && nums[k]<sum) k++;
+                res += k-j-1;
+            }
+        }
+        return res;
+    }
+};
