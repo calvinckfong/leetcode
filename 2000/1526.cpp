7f50d3d6118a196bb470c261e3df224dad9bbeac
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-10-30 22:34:30 +0800

    Create 1526.cpp

---
+++ 2000/1526.cpp
@@ -0,0 +1,12 @@
+// 1526. Minimum Number of Increments on Subarrays to Form a Target Array
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int n = target.size();
+        int res = target[0];
+        for (int i=1; i<n; i++) {
+            res += max(target[i] - target[i-1], 0);
+        }
+        return res;
+    }
+};
