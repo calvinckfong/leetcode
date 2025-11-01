117fdffe9a55283eb8fd9a41308f907445def3aa
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-11-01 19:52:25 +0800

    Update 3217.cpp

--- 4000/3217.cpp
+++ 4000/3217.cpp
@@ -1,28 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 // 3217. Delete Nodes From Linked List Present in Array
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> s;
-        for (int n : nums)  s.insert(n);
-
-        ListNode* dummy = new ListNode, *node = dummy;
-        while (head) {
-            if (!s.contains(head->val)) {
-                node->next = new ListNode(head->val);
-                node = node->next;
+        int max_num = *max_element(nums.begin(), nums.end());
+        vector<bool> set(max_num+1, false);
+        for (int x: nums)
+            set[x]= true;
+        
+        ListNode* root = new ListNode;
+        ListNode* curr = root;
+        int n = set.size();
+        while (head!=nullptr) {
+            if (head->val>=n || set[head->val]==false) {
+                curr->next = head;
+                curr = curr->next;
             }
             head = head->next;
         }
-        return dummy->next;
+        curr->next = nullptr;
+        return root->next;
     }
 };
