2803a5f36d516074bc084a6342a0d931c891d672
Calvin Fong <calvinckfong@yahoo.com.hk>
2025-12-15 22:22:29 +0800

    Create 2110.cpp

---
+++ 3000/2110.cpp
@@ -0,0 +1,19 @@
+// 2110. Number of Smooth Descent Periods of a Stock
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        int n = prices.size();
+        long long res = 1;
+        int prev = 1;
+
+        for (int i=1; i<n; i++) {
+            if (prices[i] == prices[i-1] - 1) {
+                prev++;
+            } else {
+                prev = 1;
+            }
+            res += prev;
+        }
+        return res;
+    }
+};
