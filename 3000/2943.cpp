bdd45e4e5c117b15ba784c0b59c83350b0bc4541
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-01-15 22:03:00 +0800

    Create 2943.cpp

---
+++ 3000/2943.cpp
@@ -0,0 +1,32 @@
+// 2943. Maximize Area of Square Hole in Grid
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        sort(hBars.begin(), hBars.end());
+        sort(vBars.begin(), vBars.end());
+        int hcur=1, vcur=1;
+        int hmax=1, vmax=1;
+        for (int i=1; i<hBars.size(); i++) {
+            if (hBars[i]==hBars[i-1]+1)
+                hcur++;
+            else {
+                hmax = max(hmax, hcur);
+                hcur = 1;
+            }
+        }
+        hmax = max(hmax, hcur);
+
+        for (int i=1; i<vBars.size(); i++) {
+            if (vBars[i]==vBars[i-1]+1)
+                vcur++;
+            else {
+                vmax = max(vmax, vcur);
+                vcur = 1;
+            }
+        }
+        vmax = max(vmax, vcur);
+        
+        int s = min(hmax, vmax)+1;
+        return s*s;
+    }
+};
