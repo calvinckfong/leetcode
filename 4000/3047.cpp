4e8df83c13715e5daac320d0f4a3875ac71938af
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-01-17 22:37:05 +0800

    Create 3047.cpp

---
+++ 4000/3047.cpp
@@ -0,0 +1,18 @@
+// 3047. Find the Largest Area of Square Inside Two Rectangles
+class Solution {
+public:
+    long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
+        int n = bottomLeft.size();
+        int res = 0;
+
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                int w = min(topRight[i][0], topRight[j][0]) - max(bottomLeft[i][0], bottomLeft[j][0]);
+                int h = min(topRight[i][1], topRight[j][1]) - max(bottomLeft[i][1], bottomLeft[j][1]);
+                res = max(res, min(w, h));
+            }
+        }
+
+        return 1LL * res * res;
+    }
+};
