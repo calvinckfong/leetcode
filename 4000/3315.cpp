98391e4418047f1efb7efac16ec4b9027bd5e65a
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-01-21 22:41:22 +0800

    Create 3315.cpp

---
+++ 4000/3315.cpp
@@ -0,0 +1,16 @@
+// 3315. Construct the Minimum Bitwise Array II
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        for (auto& x: nums) {
+            int res = -1;
+            int d = 1;
+            while ((x&d) != 0) {
+                res = x - d;
+                d <<= 1;
+            }
+            x = res;
+        }
+        return nums;
+    }
+};
