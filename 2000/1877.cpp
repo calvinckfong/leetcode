e0764c81e918368568771bcfbafefbb3382db0aa
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-01-24 21:58:28 +0800

    Create 1877.cpp

---
+++ 2000/1877.cpp
@@ -0,0 +1,13 @@
+// 1877. Minimize Maximum Pair Sum in Array
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        int res = 0;
+        sort(nums.begin(), nums.end());
+        int l=0, r=nums.size()-1;
+        while (l<r) {
+            res = max(res, nums[l++]+nums[r--]);
+        }
+        return res;
+    }
+};
