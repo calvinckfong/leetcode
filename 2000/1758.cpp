9491c5e31c1c8f57f402bd00eecb86c5a020ea89
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-03-05 22:47:43 +0800

    Update 1758.cpp

--- 2000/1758.cpp
+++ 2000/1758.cpp
@@ -2,9 +2,8 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int n = s.size();
         int start0 = 0, start1 = 0;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<s.size(); i++) {
             (s[i]=='0'+(i%2))?start1++:start0++;
         }
         return min(start0, start1);
