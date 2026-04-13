21d963c80b7782f21e53275b81d2a1289c1c3882
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-04-14 00:26:38 +0800

    Create 1848.cpp

---
+++ 2000/1848.cpp
@@ -0,0 +1,13 @@
+// 1848. Minimum Distance to the Target Element
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int res = nums.size();
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] == target) {
+                res = min(res, abs(i-start));
+            }
+        }
+        return res;
+    }
+};
