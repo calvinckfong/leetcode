57b3d052bfa016f876fe1442972defc47774064e
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-04-17 00:01:54 +0800

    Create 3488.cpp

---
+++ 4000/3488.cpp
@@ -0,0 +1,30 @@
+// 3488. Closest Equal Element Queries
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        vector<int> left(n), right(n);
+        unordered_map<int, int> pos;
+        for (int i=-n; i<n; i++) {
+            if (i>=0) {
+                left[i] = pos[nums[i]];
+            }
+            pos[nums[(i+n)%n]] = i;
+        }
+        pos.clear();
+        for (int i=2*n-1; i>=0; i--) {
+            if (i<n) {
+                right[i] = pos[nums[i]];
+            }
+            pos[nums[i%n]] = i;
+        }
+
+        int q = queries.size();
+        vector<int> res(q);
+        for (int i=0; i<q; i++) {
+            int x = queries[i];
+            res[i] = (x-left[x] == n) ? -1 : min(x-left[x], right[x]-x);
+        }
+        return res;
+    }
+};
