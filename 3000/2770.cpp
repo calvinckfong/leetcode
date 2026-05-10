ecc98befa6a18d1ba263159b2ba2b17dc32a002d
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-05-10 22:15:28 +0800

    Implement maximum jumps solution in 2770.cpp

---
+++ 3000/2770.cpp
@@ -0,0 +1,20 @@
+// 2770. Maximum Number of Jumps to Reach the Last Index
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MIN);
+        dp[0] = 0;
+
+        for (int i=1; i<n; i++) {
+            int& curr = dp[i];
+            for (int j=0; j<i; j++) {
+                if (abs(nums[j]-nums[i])<=target) {
+                    curr = max(curr, dp[j]+1);
+                }
+            }
+        }
+
+        return dp[n-1]<0 ? -1 : dp[n-1];
+    }
+};
