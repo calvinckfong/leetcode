d2f8808cfc1014d55e06a8ef7d65cfc7ec288940
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-05-17 00:32:34 +0800

    Create 0154.cpp

---
+++ 1000/0154.cpp
@@ -0,0 +1,21 @@
+// 154. Find Minimum in Rotated Sorted Array II
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0, r=nums.size()-1;
+        int m;
+
+        while (l<r) {
+            m = (l+r)/2;
+
+            if (nums[m]<nums[r]) {
+                r = m;
+            } else if (nums[m]>nums[r]) {
+                l = m+1;
+            } else {
+                r--;
+            }
+        }
+        return nums[l];
+    }
+};
