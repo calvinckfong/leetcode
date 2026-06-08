bb9562ef453c731ff5e30ac3c2da8eced2d983a4
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-06-08 23:51:58 +0800

    Update 2161.cpp

--- 3000/2161.cpp
+++ 3000/2161.cpp
@@ -2,19 +2,20 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> result, more, eq;
+        vector<int> res;
 
-        for (auto& n: nums) {
-            if (n<pivot)    
-                result.push_back(n);
-            else if (n>pivot)
-                more.push_back(n);
-            else
-                eq.push_back(n);
+        for (auto n: nums) {
+            if (n<pivot) res.push_back(n);
         }
-        for (auto& n: eq)   result.push_back(n);
-        for (auto& n: more) result.push_back(n);
 
-        return result;
+        for (auto n: nums) {
+            if (n==pivot) res.push_back(n);
+        }
+
+        for (auto n: nums) {
+            if (n>pivot) res.push_back(n);
+        }
+
+        return res;
     }
 };
