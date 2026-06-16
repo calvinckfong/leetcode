b98f5dbd57a404e1656300fa5c8e527175fde453
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-06-16 21:58:01 +0800

    Create 3612.cpp

---
+++ 4000/3612.cpp
@@ -0,0 +1,24 @@
+// 3612. Process String with Special Operations I
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+
+        for (auto c: s) {
+            if (c == '*') {
+                if (!res.empty()) res.pop_back();
+            }
+            else if (c == '#') {
+                res += res;
+            }
+            else if (c == '%') {
+                reverse(res.begin(), res.end());
+            }
+            else {
+                res += c;
+            }
+        }
+
+        return res;
+    }
+};
