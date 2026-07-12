2845dc881d4459c5f1e8a3c01985a26d38bcadb3
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-07-13 00:17:37 +0800

    Update 1331.cpp

--- 2000/1331.cpp
+++ 2000/1331.cpp
@@ -2,19 +2,19 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        int n = arr.size();
-        map<int, vector<int>> indices;
-        for (int i=0; i<n; i++) {
-            indices[arr[i]].push_back(i);
-        }
+        std::vector<int> sorted = arr;
+        sort(sorted.begin(), sorted.end());
+        sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
 
-        int rank = 0;
-        for (auto& p : indices) {
-            rank++;
-            for (int i: p.second) {
-                arr[i] = rank;
-            }
+        unordered_map<int, int> mp;
+        mp.reserve(sorted.size());
+        for (int i=0; i<sorted.size(); i++) {
+            mp[sorted[i]] = i+1;
         }
+        for (int& x: arr) {
+            x = mp[x];
+        }
+        
         return arr;
     }
 };
