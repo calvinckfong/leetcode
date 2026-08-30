12a4764cdf210229b0d8508c4f6dc734ba93bd6f
Calvin Fong <calvinckfong@yahoo.com.hk>
2026-08-30 23:35:19 +0800

    Create 2091.cpp

---
+++ 3000/2091.cpp
@@ -0,0 +1,14 @@
+// 2091. Removing Minimum and Maximum From Array
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        if (n<3) return n;
+
+        int minIdx = min_element(nums.begin(), nums.end()) - nums.begin();
+        int maxIdx = max_element(nums.begin(), nums.end()) - nums.begin();
+        int l = min(minIdx, maxIdx);
+        int r = max(minIdx, maxIdx);
+        return min({r+1, n-l, l+1 + n-r});
+    }
+};
